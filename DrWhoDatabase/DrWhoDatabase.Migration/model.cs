37368829de1bfe900a4b0be166025e18519a32e9
bfarmer <bfarmer@.(none)>
2013-07-07 22:30:53 -0400

    Added Migrations via NuGet.

---
+++ DrWhoDatabase/DrWhoDatabase.Migration/model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrWhoDatabase.Migration
+{
+    class DrWhoContext : DbContext
+    {
+    }
+}
