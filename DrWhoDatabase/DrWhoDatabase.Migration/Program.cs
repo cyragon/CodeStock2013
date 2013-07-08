30643bcc2e956ade8e3d3fa98fda2114d6bb3a78
Ben Farmer <dont@email.com>
2013-07-08 11:39:13 -0400

    Updated the migration project to create a script
    Updated the migration project to create a script sql file rather than
    apply the changes directly.

--- DrWhoDatabase/DrWhoDatabase.Migration/Program.cs
+++ DrWhoDatabase/DrWhoDatabase.Migration/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Migrations.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DrWhoDatabase.Migration.Migrations;
 
 namespace DrWhoDatabase.Migration
 {
@@ -10,6 +13,13 @@ namespace DrWhoDatabase.Migration
     {
         static void Main(string[] args)
         {
+            var configuration = new Configuration();
+            configuration.TargetDatabase = new System.Data.Entity.Infrastructure.DbConnectionInfo("Data Source=localhost\\sqlexpress;Initial Catalog=DrWho2013;User ID=drwho;Password=drwho", "System.Data.SqlClient");
+            var migrator = new DbMigrator(configuration);
+
+            var scriptor = new MigratorScriptingDecorator(migrator);
+            var script = scriptor.ScriptUpdate(sourceMigration: null, targetMigration: null);
+            System.IO.File.WriteAllText("script.sql", script);
         }
     }
 }
