1ba0f5a82d1a8f5db2282c331441846906a5600a
Ben Farmer <dont@email.com>
2013-07-08 15:48:09 -0400

    Added first migration
    Create the DoctorCompanion linking table

---
+++ DrWhoDatabase/DrWhoDatabase.Migration/Migrations/201307081849013_FB100-LinkingTable.cs
@@ -0,0 +1,24 @@
+namespace DrWhoDatabase.Migration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FB100LinkingTable : DbMigration
+    {
+        public override void Up()
+        {
+            // create a linking table for doctor and companion 
+            CreateTable("DoctorCompanion", c => new
+            {
+                DoctorID = c.Int(nullable: false),
+                CompanionID = c.Int(nullable: false)
+            });
+
+        }
+        
+        public override void Down()
+        {
+            DropTable("DoctorCompanion");
+        }
+    }
+}
